98fa6c7a861e481826ca60f789a0a3e38ceaaab0
Przemek Radomski <166619259+przemekradomski@users.noreply.github.com>
2025-12-12 21:46:41 +0100

    Improve error messages with colored output

--- utils/player_analysis.py
+++ utils/player_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
+from termcolor import colored
 
-# Zmienne modułowe dostępne z innych plików
 red_cards = None
 yellow_cards = None
 goals = None
@@ -9,27 +9,27 @@ assists = None
 def topRedCards(df):
     global red_cards
     if 'Red_Cards' not in df.columns:
-        print("Nie ma kolumny Red_Cards")
+        print(termcolor.colored("Nie ma kolumny Red_Cards", "red"))
         return
     red_cards = df['Red_Cards']
 
 def topYellowCards(df):
     global yellow_cards
     if 'Yellow_Cards' not in df.columns:
-        print("Nie ma kolumny Yellow_Cards")
+        print(termcolor.colored("Nie ma kolumny Yellow_Cards", "red"))
         return
     yellow_cards = df['Yellow_Cards']
 
 def topGoals(df):
     global goals
     if 'Goals' not in df.columns:
-        print("Nie ma kolumny Goals")
+        print(termcolor.colored("Nie ma kolumny Goals", "red"))
         return
     goals = df['Goals']
 
 def topAssists(df):
     global assists
     if 'Assists' not in df.columns:
-        print("Nie ma kolumny Assists")
+        print(termcolor.colored("Nie ma kolumny Assists"))
         return
     assists = df['Assists']
