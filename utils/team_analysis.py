eac184b4864be261a3851a6098bf95a6dbfda869
Przemek Radomski <166619259+przemekradomski@users.noreply.github.com>
2025-12-15 00:10:21 +0100

    Update team_analysis.py

--- utils/team_analysis.py
+++ utils/team_analysis.py
@@ -9,27 +9,27 @@ teamTopAssists = None
 def topTeamGoals(df):
     global teamTopGoals
     if 'Goals' not in df.columns or 'Team' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Goals lub Team", "red"))
+        print(colored("Nie ma kolumny Goals lub Team", "red"))
         return
     teamTopGoals = df.groupby('Team')['Goals'].sum().sort_values(ascending=False)
 
 def topTeamRedCards(df):
     global teamTopRedCards
     if 'Red_Cards' not in df.columns or 'Team' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Red_Cards lub Team", "red"))
+        print(colored("Nie ma kolumny Red_Cards lub Team", "red"))
         return
     teamTopRedCards = df.groupby('Team')['Red_Cards'].sum().sort_values(ascending=False)
 
 def topTeamYellowCards(df):
     global teamTopYellowCards
     if 'Yellow_Cards' not in df.columns or 'Team' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Yellow_Cards lub Team", "red"))
+        print(colored("Nie ma kolumny Yellow_Cards lub Team", "red"))
         return
     teamTopYellowCards = df.groupby('Team')['Yellow_Cards'].sum().sort_values(ascending=False)
 
 def topTeamAssists(df):
     global teamTopAssists
     if 'Assists' not in df.columns or 'Team' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Assists lub Team", "red"))
+        print(termcolor("Nie ma kolumny Assists lub Team", "red"))
         return
     teamTopAssists = df.groupby('Team')['Assists'].sum().sort_values(ascending=False)
