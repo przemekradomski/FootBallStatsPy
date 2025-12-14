fbf0e11725fbbbb4992d33f9aace5a3700f3a1e4
Przemek Radomski <166619259+przemekradomski@users.noreply.github.com>
2025-12-15 00:08:59 +0100

    Replace termcolor with colored for printing errors

--- utils/player_analysis.py
+++ utils/player_analysis.py
@@ -9,27 +9,27 @@ assists = None
 def topRedCards(df):
     global red_cards
     if 'Red_Cards' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Red_Cards", "red"))
+        print(colored("Nie ma kolumny Red_Cards", "red"))
         return
     red_cards = df['Red_Cards']
 
 def topYellowCards(df):
     global yellow_cards
     if 'Yellow_Cards' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Yellow_Cards", "red"))
+        print(colored("Nie ma kolumny Yellow_Cards", "red"))
         return
     yellow_cards = df['Yellow_Cards']
 
 def topGoals(df):
     global goals
     if 'Goals' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Goals", "red"))
+        print(colored("Nie ma kolumny Goals", "red"))
         return
     goals = df['Goals']
 
 def topAssists(df):
     global assists
     if 'Assists' not in df.columns:
-        print(termcolor.colored("Nie ma kolumny Assists"))
+        print(colored("Nie ma kolumny Assists"))
         return
     assists = df['Assists']
